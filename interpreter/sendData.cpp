de4ecf17e8e3cf3f910e0c7e2eab95f3031d6779
Sebastian King <sebastian-king@users.noreply.github.com>
2018-10-07 14:40:43 -0500

    Create sendData.cpp

---
+++ interpreter/sendData.cpp
@@ -0,0 +1,26 @@
+#include "pch.h"
+#include "sendData.h"
+#include <string.h>
+#include <winsock2.h>
+#include <windows.h>
+#include <iostream>
+#include <vector>
+#include <locale>
+#include <sstream>
+
+using namespace Windows::Foundation;
+using namespace Windows::Web::Http;
+
+int sentLast = 10;
+int sendVal = 20;
+void sendData(bool eye, bool jaw)
+{
+	sentLast++;
+	if (sentLast>=sendVal) {
+		auto uri = ref new Uri("http://raspberrypi.local/webdrive.php?eye=" + eye + "&jaw=" + jaw);
+		auto httpClient = ref new HttpClient();
+
+		httpClient->GetStringAsync(uri);
+		sentLast = 0;
+	}
+}
